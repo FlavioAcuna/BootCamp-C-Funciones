0f42019a63ae893f183b10b446d7eb12ba539145
FlavioAcuña <flavio.acuna.t@gmail.com>
2023-10-10 10:16:34 -0300

    new file:   Funciones.csproj 	new file:   Funciones.sln 	new file:   Program.cs 	new file:   bin/Debug/net7.0/Funciones.deps.json 	new file:   bin/Debug/net7.0/Funciones.dll 	new file:   bin/Debug/net7.0/Funciones.exe 	new file:   bin/Debug/net7.0/Funciones.pdb 	new file:   bin/Debug/net7.0/Funciones.runtimeconfig.json 	new file:   obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   obj/Debug/net7.0/Funciones.AssemblyInfo.cs 	new file:   obj/Debug/net7.0/Funciones.AssemblyInfoInputs.cache 	new file:   obj/Debug/net7.0/Funciones.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net7.0/Funciones.GlobalUsings.g.cs 	new file:   obj/Debug/net7.0/Funciones.assets.cache 	new file:   obj/Debug/net7.0/Funciones.csproj.AssemblyReference.cache 	new file:   obj/Debug/net7.0/Funciones.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net7.0/Funciones.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net7.0/Funciones.dll 	new file:   obj/Debug/net7.0/Funciones.genruntimeconfig.cache 	new file:   obj/Debug/net7.0/Funciones.pdb 	new file:   obj/Debug/net7.0/apphost.exe 	new file:   obj/Debug/net7.0/ref/Funciones.dll 	new file:   obj/Debug/net7.0/refint/Funciones.dll 	new file:   obj/Funciones.csproj.nuget.dgspec.json 	new file:   obj/Funciones.csproj.nuget.g.props 	new file:   obj/Funciones.csproj.nuget.g.targets 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Funcion en Js
+function nombre(param1, param2)
+{
+    return valor;
+}
+*/
+static int SumaDosNumeros(int num1, int num2)
+{
+    int total = num1 + num2;
+    return total;
+};
+//Para no tener un valor de retorno 
+static void ImprimiendoHola(string nombre)
+{
+    Console.WriteLine($"Hola, Como estas {nombre}!");
+}
+
+int resultado = SumaDosNumeros(30, 20);
+Console.WriteLine(resultado);
+ImprimiendoHola("Alex");
